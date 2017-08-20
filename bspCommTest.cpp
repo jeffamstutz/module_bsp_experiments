27fc8912090537014e3154e35ed0ae77612904b1
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2017-08-20 15:45:14 +0000

    start with "hello" example from Bulk

--- bspCommTest.cpp
+++ bspCommTest.cpp
@@ -26,9 +26,53 @@
 
 // Bulk library
 #include "bulk/bulk.hpp"
-#include "bulk/backends/thread/environment.hpp"
+
+#if 1
+#  include <bulk/backends/thread/thread.hpp>
+using environment = bulk::thread::environment;
+#else
+#  include <bulk/backends/mpi/mpi.hpp>
+using environment = bulk::mpi::environment;
+#endif
 
 int main()
 {
-  std::cout << "hello world!" << std::endl;
+  environment env;
+
+  env.spawn(env.available_processors(), [](bulk::world &world) {
+    int s = world.rank();
+    int p = world.active_processors();
+
+    world.log("Hello, world %d/%d", s, p);
+
+    auto a               = bulk::var<int>(world);
+    a(world.next_rank()) = s;
+    world.sync();
+    // ... the local a is now updated
+    world.log("%d got put %d", s, a.value());
+
+    auto b = a(world.next_rank()).get();
+    world.sync();
+    // ... b.value() is now available
+
+    // coarrays are distributed arrays, each processor has an array to which
+    // other processors can write
+    auto xs                  = bulk::coarray<int>(world, 10);
+    xs(world.next_rank())[3] = s;
+
+    // messages can be passed to queues that specify a tag type, and a
+    // content type
+    auto q = bulk::queue<int, float>(world);
+    for (int t = 0; t < p; ++t) {
+      q(t).send(s, 3.1415f);  // send (s, pi) to processor t
+    }
+    world.sync();
+
+    // Messages are now available in q
+    for (auto[tag, content] : q) {
+      world.log("%d got sent %d, %f", s, tag, content);
+    }
+  });
+
+  return 0;
 }
